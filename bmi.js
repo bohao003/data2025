7c8d832e073225ed4aab5cec602868a991acf5eb
bohao003 <mo20031024@gmail.com>
2025-09-23 16:03:51 +0800

    250923002

--- bmi.js
+++ bmi.js
@@ -1,7 +1,7 @@
 const readline = require('readline-sync');
 
-var weight= readline.question("Your weight ? (Kg)");
-var height= readline.question("Your height ? (cm)");
+var weight= readline.questionFloat("Your weight ? (Kg)");
+var height= readline.questionInt("Your height ? (cm)");
 var bmi = weight/((height/100)**2);
 
 console.log("Hello! Your BMI value is"+bmi);
