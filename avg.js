b57c734f2e3ca6ddf7698fa22a8acb1439afd08c
bohao003 <mo20031024@gmail.com>
2025-09-30 14:57:14 +0800

    250930

---
+++ avg.js
@@ -0,0 +1,13 @@
+function avg(score){
+    var sum=0, average=0;
+    for(var i=0;i<score.length;i++)
+        {
+            sum += score[i];
+        }
+        if(score.length>0)
+            average=sum/score.length;
+        return average;
+}
+
+var Score=[50,20,30,15,16,21,33,11,9,27];
+console.log(Score.toString+" average:"+ avg(Score))
