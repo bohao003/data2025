7827605b78f5c0524ed05841cc465c4183e2fdf0
bohao003 <mo20031024@gmail.com>
2025-10-07 14:12:05 +0800

    25107001

---
+++ sum_v2.js
@@ -0,0 +1,21 @@
+// calc 1-2+3-4+...n
+function sum2(n){
+    var sum=0;
+    if(n<0){
+        console.log("error! n must>0");
+        return null;
+    }else{
+        for(var i=1;i<=n;i++){
+            if(i%2==0)
+                sum -= i;
+            else
+                sum += i;
+        }
+        return sum;
+    }
+}
+
+
+//test 
+var n=100;
+console. log("1-2+3-4+..."+n+"="+sum2(n));
